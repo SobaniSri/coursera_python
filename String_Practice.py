cba5d3e1c4245045cb55837214fad9bcd3df3cfe
Sobani <sribany.sj@gmail.com>
2021-09-11 15:07:21 +0530

    Add string practice file

---
+++ String_Practice.py
@@ -0,0 +1,55 @@
+a = 1
+print(a)
+
+b = 2
+print(b)
+
+c = a + b
+print(c)
+
+print("-----------")
+
+a = "1"
+print(a)
+
+b = "2"
+print(b)
+
+c = a + b
+print(c)
+
+print("-----------")
+
+#Slicing to print out the first three elements of d:
+d = "ABCDEFG"
+print(d[0:3])
+
+print("-----------")
+
+#To print out every second character of the string e:
+e = 'clocrkr1e1c1t'
+print(e[::2])
+
+print("-----------")
+
+#To print out a backslash:
+print("\\")
+
+print("-----------")
+
+#Convert the variable f to uppercase:
+f = "You are wrong"
+print(f.upper())
+
+print("-----------")
+
+#find the first index of the sub-string "snow":
+g = "Mary had a little lamb Little lamb, little lamb Mary had a little lamb \
+Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+Everywhere that Mary went The lamb was sure to go"
+print(g.find("snow"))
+
+print("-----------")
+
+#To replace the sub-string "Mary" with "Bob":
+print(g.replace("Mary","Bob"))
