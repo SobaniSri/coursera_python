163af54e58e1113a22962a2268ea3d565d16b547
Sobani <sribany.sj@gmail.com>
2021-09-18 21:56:26 +0530

    Add list practice

---
+++ list_practice.py
@@ -0,0 +1,29 @@
+#Create a list a_list
+a_list = [ 1, "hello", [1,2,3], True]
+
+print(a_list)
+
+print("------------")
+
+#Find the value stored at index 1 of a_list
+
+print(a_list[1])
+
+print("------------")
+
+#Retrieve the elements stored at index 1, 2 and 3 of a_list.
+
+print(a_list[1:4])
+
+print("------------")
+
+#Concatenate the following lists A = [1, 'a'] and B = [2, 1, 'd']:
+
+A = [1, 'a']
+
+B = [2, 1, 'd']
+
+c = A + B
+
+print(c)
+
